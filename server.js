bc1492f252f498bb949c09d2b56ad5f8f4d7a523
NaveedRehman12 <60893649+NaveedRehman12@users.noreply.github.com>
2026-01-13 18:31:40 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -76,11 +76,14 @@ app.use('/api/notifications', notificationRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('Connected to MongoDB');
-        server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     })
     .catch(err => {
         console.error('MongoDB connection error:', err);
     });
+
+module.exports = server;
